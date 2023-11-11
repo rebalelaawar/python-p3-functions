b967754fe7bd60db3bc2791deb8be9263c002393
rebalelaawar <rebalelaawar@gmail.com>
2023-11-11 16:31:26 -0500

    Python p3 functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    print(45+55)
+
+
+
+    
 
 def halve(number):
-    pass
+    return number/2
+    print(15/2)
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+
+
+
